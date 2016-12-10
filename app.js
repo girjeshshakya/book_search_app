2c4b39fbf275464fdb3fa13af12d69ac07f22471
avnesh shakya <avnesh shakya>
2016-12-11 00:48:35 +0530

    dynamic data handling added

--- app.js
+++ app.js
@@ -1,23 +1,17 @@
 var express = require('express');
 var app = express();
 app.set('view engine', 'ejs');
+var routes = require('./routes');
+var path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Routes 
-// home routes
-app.get('/', function (req, res) {
-    res.render('home', {
-        title:'book library',
-        books:['first book', 'second book', 'third book']
-    });
-});
-//book single
-app.get('/Book/:book_number?', function (req, res) {
-    var book_num = req.params.book_number;
-    res.send("this is server response for Book" + book_num);
-});
-//not found
-app.get('*', function (req, res) {
-    res.send("this is not the page yiou are looking for");
-});
+// homes
+app.get('/', routes.home);
+//book_single
+app.get('/Book/:book_number?', routes.book_single);
+//notfound
+app.get('*', routes.notFound);
 app.listen(3000, function () {
     console.log("the application is running on localhost");
 });
---
+++ routes/index.js
@@ -0,0 +1,19 @@
+var bookJson = require('../book.json');
+// Routes 
+// home
+exports.home = function (req, res) {
+    var books = bookJson.books;
+    res.render('home', {
+        title: 'book library'
+        , books: books
+    });
+};
+//book_single
+exports.book_single = function (req, res) {
+    var book_num = req.params.book_number;
+    res.send("this is server response for Book" + book_num);
+};
+//notFound
+exports.notFound = function (req, res) {
+    res.send("this is not the page yiou are looking for");
+};
